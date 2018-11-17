758cf3ddcc6ff8ba8a1d2603a378acef4fbe1639
Manuel Morales <ppjuan_94@hotmail.com>
2018-11-17 12:53:14 -0600

    Configuraciones iniciales

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,36 +1,36 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+//Imports ::
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+//vForm
+import { Form, HasError, AlertError } from 'vform';
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//Moments
+import moment from 'moment';
+Vue.filter('timeDate', function(created){
+    return moment(created).format('MMMM Do YYYY');
+ })
 
-// const files = require.context('./', true, /\.vue$/i)
+//VueRouter
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+let routes = [
 
-// files.keys().map(key => {
-//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
-// })
+]
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const router = new VueRouter({
+    mode: 'history',
+    routes 
+})
 
+//Componentes ::
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
