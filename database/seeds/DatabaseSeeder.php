767a80de96a94e6247e02547d00246780dfb2983
Manuel Morales <ppjuan_94@hotmail.com>
2018-11-17 13:17:10 -0600

    Creación de seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Pizza Just',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('secret'),
+            'role' => 'Admin'
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Pizza Just',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('secret'),
+            'role' => 'User'
+        ]);
+
+        DB::table('pizza')->insert([
+            'name_pizza'  => 'Peperoni',
+            'price' => '150'
+        ]);
+
+        DB::table('pizza')->insert([
+            'name_pizza'  => 'Combinada',
+            'price' => '175'
+        ]);
     }
 }
