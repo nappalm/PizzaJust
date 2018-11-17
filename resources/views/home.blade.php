319a89159162f4658ebe2246bfcdd3042ae86aa8
Manuel Morales <ppjuan_94@hotmail.com>
2018-11-17 13:08:04 -0600

    Maquetado y estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+<nav class="navbar navbar-expand-lg nav-pizza-dashboard">
+    <div class="container-fluid">
+        <a class="navbar-brand p-0" href="#"><img src="img/logo_min_text.png" height="30"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item ">
+                <router-link to="/neworder" class="nav-link nav-s-item"><span class="fas fa-receipt"></span> Ordenar</router-link>
+            </li>
+            @can('Admin')
+            <li class="nav-item">
+                <router-link to="/orders" class="nav-link nav-s-item " href="#"><span class="fas fa-calendar-check"></span> Lista de ordenes</router-link>
+            </li>
+            @endcan
+            <li class="nav-item">
+                <a class="nav-link nav-s-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Cerrar Sesión') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+            
+            </ul>
         </div>
     </div>
-</div>
+</nav>
+<div class="container-fluid pt-5">
+    <router-view></router-view>
+</div> 
 @endsection
