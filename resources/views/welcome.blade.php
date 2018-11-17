69e29ecfad6daeecee2198b01fc8a9b5d7a9fb77
Manuel Morales <ppjuan_94@hotmail.com>
2018-11-17 17:06:19 -0600

    Ups!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{mix('css/app.css')}}">
         <title>PizzaJust</title>
 
     </head>
     <body  class="bg__darkpizza">
-    <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12 text-center p-5">
-                    <img src="img/logo.png" alt="">
-                </div>
+        <div id="app">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 text-center p-5">
+                        <img src="img/logo.png" alt="">
+                    </div>
                 <div class="col-md-12 text-center">
                     <h1>Bienvenido a PizzaJust!</h1>
                     <p>Esta area pertenece solo a personal administrativo.</p>
@@ -20,7 +22,8 @@
                 </div>
             </div>
         </div>
-        <script src="{{mix('js/app.js')}}"></script>
+    </div>
+    <script src="{{mix('js/app.js')}}"></script>
     </body>
     </body>
 </html>
